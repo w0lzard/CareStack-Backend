1dea9a395f4c5ebcc8cb83be76a44fff18d480d9
w0lzard <kpsingj6@gmail.com>
2026-07-29 18:46:48 +0530

    feat: add Specialization enum for medical specialties

---
+++ src/main/java/com/ryuken/carestack/entity/Specialization.java
@@ -0,0 +1,11 @@
+package com.ryuken.carestack.entity;
+
+public enum Specialization {
+    CARDIOLOGY,
+    NEUROLOGY,
+    ORTHOPEDICS,
+    PEDIATRICS,
+    GENERAL_MEDICINE,
+    DERMATOLOGY,
+    ONCOLOGY
+}
