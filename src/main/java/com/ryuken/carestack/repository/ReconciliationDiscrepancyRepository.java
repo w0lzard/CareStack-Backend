9ea8bcc1059c1c39ac9a891c5c8c285cfd2c42c3
w0lzard <kpsingj6@gmail.com>
2026-07-29 17:27:36 +0530

    feat: add ReconciliationDiscrepancyRepository

---
+++ src/main/java/com/ryuken/carestack/repository/ReconciliationDiscrepancyRepository.java
@@ -0,0 +1,7 @@
+package com.ryuken.carestack.repository;
+
+import com.ryuken.carestack.entity.ReconciliationDiscrepancy;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ReconciliationDiscrepancyRepository extends JpaRepository<ReconciliationDiscrepancy, Long> {
+}
