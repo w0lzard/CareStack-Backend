e088b02b7c81911c38890485036bf942a1dbabef
w0lzard <kpsingj6@gmail.com>
2026-07-29 17:27:36 +0530

    test: add CareStackApplicationTests

---
+++ src/test/java/com/ryuken/carestack/CareStackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ryuken.carestack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CareStackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
