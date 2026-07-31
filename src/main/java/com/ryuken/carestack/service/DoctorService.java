4483ea5194e672b6b551e37681bfe5d731a0b6b0
w0lzard <kpsingj6@gmail.com>
2026-07-31 12:46:23 +0530

    feat: add DoctorService interface

--- src/main/java/com/ryuken/carestack/service/DoctorService.java
+++ src/main/java/com/ryuken/carestack/service/DoctorService.java
@@ -1,4 +1,14 @@
 package com.ryuken.carestack.service;
 
-public class DoctorService {
+import com.ryuken.carestack.dto.DoctorDto;
+import com.ryuken.carestack.entity.Specialization;
+
+import java.util.List;
+
+public interface DoctorService {
+    DoctorDto createDoctor(DoctorDto dto);
+    DoctorDto getDoctorById(Long id);
+    List<DoctorDto> getDoctorsBySpecialization(Specialization specialization);
+    DoctorDto updateDoctor(Long id, DoctorDto dto);
+    void deactivateDoctor(Long id);
 }
