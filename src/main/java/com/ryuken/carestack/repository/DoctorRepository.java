5e872c51ae3d4ad4b2af444fc69f00aafdf94454
w0lzard <kpsingj6@gmail.com>
2026-07-31 12:46:23 +0530

    feat: add DoctorRepository with query methods for doctors

--- src/main/java/com/ryuken/carestack/repository/DoctorRepository.java
+++ src/main/java/com/ryuken/carestack/repository/DoctorRepository.java
@@ -1,4 +1,12 @@
 package com.ryuken.carestack.repository;
 
-public interface DoctorRepository {
+import com.ryuken.carestack.entity.Doctor;
+import com.ryuken.carestack.entity.Specialization;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface DoctorRepository extends JpaRepository<Doctor, Long> {
+    List<Doctor> findBySpecializationAndActiveTrue(Specialization specialization);
 }
+
