40c92caa8740594dae9dcc61c9b733e3305e5158
w0lzard <kpsingj6@gmail.com>
2026-07-31 12:46:23 +0530

    feat: add AppointmentWriter interface for appointment mutations

--- src/main/java/com/ryuken/carestack/service/AppointmentWriter.java
+++ src/main/java/com/ryuken/carestack/service/AppointmentWriter.java
@@ -1,4 +1,11 @@
 package com.ryuken.carestack.service;
 
+import com.ryuken.carestack.dto.AppointmentDto;
+
+import java.util.List;
+
 public interface AppointmentWriter {
+    AppointmentDto bookAppointment(AppointmentDto dto);
+    AppointmentDto cancelAppointment(Long id);
+    AppointmentDto markCompleted(Long id);
 }
