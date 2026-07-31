d3686666c2575b6a52aee18a43842c8f76583ba5
w0lzard <kpsingj6@gmail.com>
2026-07-31 12:46:23 +0530

    feat: add PatientService interface

--- src/main/java/com/ryuken/carestack/service/PatientService.java
+++ src/main/java/com/ryuken/carestack/service/PatientService.java
@@ -1,4 +1,13 @@
 package com.ryuken.carestack.service;
 
+import com.ryuken.carestack.dto.PatientDto;
+
+import java.util.List;
+
 public interface PatientService {
+    PatientDto createPatient(PatientDto dto);
+    PatientDto getPatientById(Long id);
+    List<PatientDto> getAllPatients();
+    PatientDto updatePatient(Long id, PatientDto dto);
+    void deletePatient(Long id);
 }
