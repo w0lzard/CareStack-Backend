049a6c9629069d0e169043650fa41b12a6289cea
w0lzard <kpsingj6@gmail.com>
2026-07-31 12:46:23 +0530

    feat: add PaymentRepository with query methods for payments

--- src/main/java/com/ryuken/carestack/repository/PaymentRepository.java
+++ src/main/java/com/ryuken/carestack/repository/PaymentRepository.java
@@ -1,4 +1,7 @@
 package com.ryuken.carestack.repository;
 
-public interface PaymentRepository {
+import com.ryuken.carestack.entity.Payment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PaymentRepository extends JpaRepository<Payment, Long> {
 }
