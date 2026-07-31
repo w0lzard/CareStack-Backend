ed9f739ab58f2a1abcaccf663bff3739b6421be7
w0lzard <kpsingj6@gmail.com>
2026-07-31 12:46:23 +0530

    feat: add PaymentService interface

--- src/main/java/com/ryuken/carestack/service/PaymentService.java
+++ src/main/java/com/ryuken/carestack/service/PaymentService.java
@@ -1,4 +1,9 @@
 package com.ryuken.carestack.service;
 
+import com.ryuken.carestack.dto.PaymentRequestDto;
+import com.ryuken.carestack.dto.PaymentResponseDto;
+
 public interface PaymentService {
+    PaymentResponseDto processPayment(PaymentRequestDto request);
+    PaymentResponseDto getPaymentById(Long id);
 }
