3f9d454b467eefa08ea67e0ebe70342b5d2b883c
w0lzard <kpsingj6@gmail.com>
2026-07-31 12:46:23 +0530

    feat: add AppointmentReader interface for appointment lookups

--- src/main/java/com/ryuken/carestack/service/AppointmentReader.java
+++ src/main/java/com/ryuken/carestack/service/AppointmentReader.java
@@ -1,4 +1,10 @@
 package com.ryuken.carestack.service;
 
-public interface AppoitnmentReader {
+import com.ryuken.carestack.dto.AppointmentDto;
+
+import java.util.List;
+
+public interface AppointmentReader {
+    AppointmentDto getAppointmentById(Long id);
+    List<AppointmentDto> getAppointmentsForPatient(Long patientId);
 }
