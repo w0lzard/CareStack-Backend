213f475300a4cb6e0bb38e0b0c80304897c7aad1
w0lzard <kpsingj6@gmail.com>
2026-07-31 12:46:23 +0530

    feat: add PatientRepository with query methods for patients

--- src/main/java/com/ryuken/carestack/repository/PatientRepository.java
+++ src/main/java/com/ryuken/carestack/repository/PatientRepository.java
@@ -1,4 +1,10 @@
 package com.ryuken.carestack.repository;
 
-public interface PatientRepository {
+import com.ryuken.carestack.entity.Patient;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface PatientRepository extends JpaRepository<Patient, Long> {
+    Optional<Patient> findByPhoneNumber(String phoneNumber);
 }
