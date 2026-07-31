7329f8ab4c638a749f37219f1b834329d5d3e0e9
w0lzard <kpsingj6@gmail.com>
2026-07-31 12:46:24 +0530

    feat: add PaymentController REST endpoints

--- src/main/java/com/ryuken/carestack/controller/PaymentController.java
+++ src/main/java/com/ryuken/carestack/controller/PaymentController.java
@@ -1,4 +1,14 @@
 package com.ryuken.carestack.controller;
 
+import com.ryuken.carestack.service.PaymentService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/payments")
+@RequiredArgsConstructor
 public class PaymentController {
+
+    private final PaymentService paymentService;
 }
