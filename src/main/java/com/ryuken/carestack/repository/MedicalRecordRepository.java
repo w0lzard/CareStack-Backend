10a72d0f3b75f4bf8f985c5ea4b01313f380b8c8
w0lzard <kpsingj6@gmail.com>
2026-07-31 12:46:23 +0530

    feat: add MedicalRecordRepository with patient lookup

--- src/main/java/com/ryuken/carestack/repository/MedicalRecordRepository.java
+++ src/main/java/com/ryuken/carestack/repository/MedicalRecordRepository.java
@@ -1,4 +1,10 @@
 package com.ryuken.carestack.repository;
 
-public interface MedicalRecordRepository {
+import com.ryuken.carestack.entity.MedicalRecord;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface MedicalRecordRepository extends JpaRepository<MedicalRecord, Long> {
+    List<MedicalRecord> findByPatientId(Long patientId);
 }
