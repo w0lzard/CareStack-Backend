6552937e8a3a13f4c17c02ada99834663c6e0871
w0lzard <kpsingj6@gmail.com>
2026-07-31 12:46:23 +0530

    feat: add PaymentStrategy interface for payment method strategies

--- src/main/java/com/ryuken/carestack/service/PaymentStrategy.java
+++ src/main/java/com/ryuken/carestack/service/PaymentStrategy.java
@@ -1,4 +1,11 @@
 package com.ryuken.carestack.service;
 
+import com.ryuken.carestack.dto.PaymentResult;
+import com.ryuken.carestack.entity.PaymentMethod;
+
+import java.math.BigDecimal;
+
 public interface PaymentStrategy {
+    PaymentMethod supportedMethod();
+    PaymentResult charge(BigDecimal amount, String payerReference);
 }
