c6b42e018da60b04416255c493dcfb2e1038541a
w0lzard <kpsingj6@gmail.com>
2026-07-31 12:46:24 +0530

    feat: add AppointmentReminderJob scheduled task

--- src/main/java/com/ryuken/carestack/scheduler/AppointmentReminderJob.java
+++ src/main/java/com/ryuken/carestack/scheduler/AppointmentReminderJob.java
@@ -1,4 +1,10 @@
 package com.ryuken.carestack.scheduler;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor@Slf4j
 public class AppointmentReminderJob {
 }
