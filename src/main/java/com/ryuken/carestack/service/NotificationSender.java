9e64edc39864dc5487273b8664fd837aff718df3
w0lzard <kpsingj6@gmail.com>
2026-07-31 12:46:23 +0530

    feat: add NotificationSender interface

--- src/main/java/com/ryuken/carestack/service/NotificationSender.java
+++ src/main/java/com/ryuken/carestack/service/NotificationSender.java
@@ -1,4 +1,8 @@
 package com.ryuken.carestack.service;
 
 public interface NotificationSender {
+    void send(String recipient, String message);
+
+    /** Lets the caller pick a sender for a given contact without knowing concrete types. */
+    boolean supports(String recipient);
 }
