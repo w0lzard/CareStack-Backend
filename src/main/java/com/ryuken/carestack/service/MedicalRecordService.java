a51c4dddc51e60486732f053592475350460d2a6
w0lzard <kpsingj6@gmail.com>
2026-07-31 12:46:23 +0530

    feat: add MedicalRecordService interface

--- src/main/java/com/ryuken/carestack/service/MedicalRecordService.java
+++ src/main/java/com/ryuken/carestack/service/MedicalRecordService.java
@@ -1,4 +1,10 @@
 package com.ryuken.carestack.service;
 
+import com.ryuken.carestack.dto.MedicalRecordDto;
+
+import java.util.List;
+
 public interface MedicalRecordService {
+    MedicalRecordDto createRecord(MedicalRecordDto dto);
+    List<MedicalRecordDto> getRecordsForPatient(Long patientId);
 }
