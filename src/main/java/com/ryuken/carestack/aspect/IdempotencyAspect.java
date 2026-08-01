a119222768d94b33a159c81cd174b9ceb8db7d67
w0lzard <kpsingj6@gmail.com>
2026-08-01 12:20:21 +0530

    feat: update application configuration to use environment variables and add local profile

--- src/main/java/com/ryuken/carestack/aspect/IdempotencyAspect.java
+++ src/main/java/com/ryuken/carestack/aspect/IdempotencyAspect.java
@@ -85,7 +85,7 @@ public class IdempotencyAspect {
             MessageDigest digest = MessageDigest.getInstance("SHA-256");
             byte[] bytes = digest.digest(input.getBytes());
             StringBuilder sb = new StringBuilder();
-            for (byte b : bytes) {
+            for  (byte b : bytes) {
                 sb.append(String.format("%02x", b));
             }
             return sb.toString();
